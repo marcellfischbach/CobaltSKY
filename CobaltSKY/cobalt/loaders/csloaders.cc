8abf4b446d3853d18ea62478f4ace92cdbd97019
Marcell Fischbach <marcellfischbach@googlemail.com>
2018-03-06 16:35:49 +0100

    Removed all xml based loaders.

--- CobaltSKY/cobalt/loaders/csloaders.cc
+++ CobaltSKY/cobalt/loaders/csloaders.cc
@@ -2,27 +2,27 @@
 #include <cobalt/loaders/csloaders.hh>
 #include <cobalt/loaders/csblueprintcsfloader.hh>
 #include <cobalt/loaders/cscollisionassetcsfloader.hh>
-#include <cobalt/loaders/cscollisionassetxmlloader.hh>
+//#include <cobalt/loaders/cscollisionassetxmlloader.hh>
 #include <cobalt/loaders/cspngimageassetloader.hh>
 #include <cobalt/loaders/cspngimagefileloader.hh>
 #include <cobalt/loaders/cspropertysettercsfloader.hh>
 #include <cobalt/loaders/csmaterialassetcsfloader.hh>
-#include <cobalt/loaders/csmaterialassetxmlloader.hh>
+//#include <cobalt/loaders/csmaterialassetxmlloader.hh>
 #include <cobalt/loaders/csmaterialdefassetcsfloader.hh>
-#include <cobalt/loaders/csmaterialdefassetxmlloader.hh>
+//#include <cobalt/loaders/csmaterialdefassetxmlloader.hh>
 #include <cobalt/loaders/csmeshassetcsfloader.hh>
-#include <cobalt/loaders/csmeshassetxmlloader.hh>
+//#include <cobalt/loaders/csmeshassetxmlloader.hh>
 #include <cobalt/loaders/cssamplerassetcsfloader.hh>
-#include <cobalt/loaders/cssamplerassetxmlloader.hh>
+//#include <cobalt/loaders/cssamplerassetxmlloader.hh>
 #include <cobalt/loaders/csshadergraphassetcsfloader.hh>
-#include <cobalt/loaders/csshadergraphassetxmlloader.hh>
+//#include <cobalt/loaders/csshadergraphassetxmlloader.hh>
 #include <cobalt/loaders/csskinnedmeshassetcsfloader.hh>
-#include <cobalt/loaders/csskinnedmeshassetxmlloader.hh>
+//#include <cobalt/loaders/csskinnedmeshassetxmlloader.hh>
 #include <cobalt/loaders/csstaticmeshassetloader.hh>
 #include <cobalt/loaders/csstaticmeshloader.hh>
 #include <cobalt/loaders/cssubmeshassetloader.hh>
 #include <cobalt/loaders/cstextureassetcsfloader.hh>
-#include <cobalt/loaders/cstextureassetxmlloader.hh>
+//#include <cobalt/loaders/cstextureassetxmlloader.hh>
 
 
 
@@ -41,22 +41,22 @@ void csLoaders::Register(csResourceManager *mgr)
   mgr->RegisterLoader(new csBlueprintCSFLoader());
 
   mgr->RegisterLoader(new csCollisionAssetCSFLoader());
-  mgr->RegisterLoader(new csCollisionAssetXMLLoader());
+//  mgr->RegisterLoader(new csCollisionAssetXMLLoader());
   mgr->RegisterLoader(new csMaterialAssetCSFLoader());
-  mgr->RegisterLoader(new csMaterialAssetXMLLoader());
+//  mgr->RegisterLoader(new csMaterialAssetXMLLoader());
   mgr->RegisterLoader(new csMaterialDefAssetCSFLoader());
-  mgr->RegisterLoader(new csMaterialDefAssetXMLLoader());
+//  mgr->RegisterLoader(new csMaterialDefAssetXMLLoader());
   mgr->RegisterLoader(new csMeshAssetCSFLoader());
-  mgr->RegisterLoader(new csMeshAssetXMLLoader());
+//  mgr->RegisterLoader(new csMeshAssetXMLLoader());
   mgr->RegisterLoader(new csPropertySetterCSFLoader());
   mgr->RegisterLoader(new csSamplerAssetCSFLoader());
-  mgr->RegisterLoader(new csSamplerAssetXMLLoader());
+//  mgr->RegisterLoader(new csSamplerAssetXMLLoader());
   mgr->RegisterLoader(new csShaderGraphAssetCSFLoader());
-  mgr->RegisterLoader(new csShaderGraphAssetXMLLoader());
+//  mgr->RegisterLoader(new csShaderGraphAssetXMLLoader());
   mgr->RegisterLoader(new csSkinnedMeshAssetCSFLoader());
-  mgr->RegisterLoader(new csSkinnedMeshAssetXMLLoader());
+//  mgr->RegisterLoader(new csSkinnedMeshAssetXMLLoader());
   mgr->RegisterLoader(new csTextureAssetCSFLoader());
-  mgr->RegisterLoader(new csTextureAssetXMLLoader());
+//  mgr->RegisterLoader(new csTextureAssetXMLLoader());
 
 
   // image loaders
